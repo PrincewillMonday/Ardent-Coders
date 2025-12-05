618955e9ae59d3e0ecbd5c3e0a2c0ab475099235
Princewill Monday <princewillmonday1999@gmail.com>
2025-12-05 16:14:11 +0100

    git commit -m "Centered project cards and blog items; Added FAQ section styles"

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 function toggleMenu() {
         document.getElementById("navLinks").classList.toggle("active");
     }
+
+    let faqButtons = document.querySelectorAll(".faq-btn");
+
+faqButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        const content = btn.nextElementSibling;
+
+        btn.classList.toggle("active");
+
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+});
